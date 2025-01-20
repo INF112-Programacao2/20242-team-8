9350c0255f14e268ef4641153458046f962680b3
Carlos <163069442+G3L0UR3N@users.noreply.github.com>
2025-01-20 13:17:41 -0300

    Update TipoProduto.cpp

--- TipoProduto.cpp
+++ TipoProduto.cpp
@@ -1,5 +1,28 @@
 //
-// Created by viniciuskl on 06/01/25.
+// Created by Carlos_Pereira on 18/01/2025
 //
 
 #include "TipoProduto.h"
+#include <iostream>
+
+TipoProduto::TipoProduto(std::string _nome) {
+    nome = _nome;
+};
+
+TipoProduto::TipoProduto() {}
+
+void TipoProduto::obterDados() { //getDados
+    std::cout << "Nome do produto: " << nome << '\n';
+    return;
+};
+
+void TipoProduto::set_nome(std::string txt) {
+    nome = txt;
+    return;
+};
+
+std::string TipoProduto::get_nome() {
+    return nome;
+};
+
+TipoProduto::~TipoProduto() {};
