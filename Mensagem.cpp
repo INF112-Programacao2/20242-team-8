d93b68f663a78e38bf4bbc0913060d2eb7f02275
Carlos <163069442+G3L0UR3N@users.noreply.github.com>
2025-01-20 13:15:16 -0300

    Base de Mensagem.cpp

--- Mensagem.cpp
+++ Mensagem.cpp
@@ -1,5 +1,18 @@
 //
-// Created by viniciuskl on 06/01/25.
+// Created by Carlos_Pereira on 18/01/2025
 //
 
 #include "Mensagem.h"
+#include <iostream>
+
+/*
+Mensagem::Mensagem() {};
+
+void Mensagem::exibirMensagem() {};
+
+void Mensagem::set_conteudo(std::string txt) {};
+
+std::string Mensagem::get_conteudo() {};
+
+Mensagem::~Mensagem() {};
+*/
