9f886be7d6eb13d5eaad54c539abcaf51581e94a
Vinicius <viniciuskusterlodi@gmail.com>
2025-01-22 17:22:32 -0300

    Classe interface

---
+++ Interface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by viniciuskl on 22/01/25.
+//
+
+#include "Interface.h"
---
+++ Interface.h
@@ -0,0 +1,30 @@
+//
+// Created by viniciuskl on 22/01/25.
+//
+
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+
+class Interface {
+public:
+    void exibirMenuPrincipal();
+
+    void exibirSubcategoriaPecas();
+    void subcategoriaAdicionarProduto();
+
+    void exibirSubcategoriaVeiculos();
+    void subcategoriaModificarVeiculo();
+
+    void exibirSubcategoriaFuncionarios();
+    void subcategoriaModificarFuncionarios();
+
+    void exibirSubcategoriaManutencao();
+    void subcategoriaModificarManutencao();
+
+};
+
+
+
+#endif //INTERFACE_H
