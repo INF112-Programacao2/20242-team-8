8faacebfebc1eeca9db8835c5fa316a56cde7cf0
Cleber1303 <cleberoliveijr@gmail.com>
2025-01-29 17:19:27 -0300

    Add files via upload

---
+++ TipoProduto.h
@@ -0,0 +1,22 @@
+//
+// Created by viniciuskl on 06/01/25.
+//
+
+#ifndef TIPOPRODUTO_H
+#define TIPOPRODUTO_H
+#include <string>
+
+class TipoProduto {
+private:
+    std::string nome;
+public:
+    explicit TipoProduto(std::string _nome);
+    virtual void obterDados(); //get Tipo_Produto
+    void set_nome(const std::string &txt);
+    std::string get_nome();
+    ~TipoProduto();
+};
+
+
+
+#endif //TIPOPRODUTO_H
